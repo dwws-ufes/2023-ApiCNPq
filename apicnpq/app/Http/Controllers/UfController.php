502d3a7b59f899c3af160dc8c7116dd2e74e848b
vilkerpessin <vilker.pessin@edu.ufes.br>
2023-07-13 18:26:18 -0300

    ajustes nos arquivos de rota, controller e view para incorporar campo de linked Data. Busca na dbpedia

--- apicnpq/app/Http/Controllers/UfController.php
+++ apicnpq/app/Http/Controllers/UfController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Uf;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
 
 class UfController extends Controller
 {
@@ -16,6 +18,49 @@ class UfController extends Controller
 
         return view('ufs.index')->with('ufs', $ufs);
     }
+
+    /**
+     * buscar população dbpedia
+     */
+
+     public function getResumo($uf)
+{
+    // Fazer a requisição à DBpedia e obter o resumo do UF
+    // Você pode usar bibliotecas como cURL ou Guzzle para fazer a requisição
+
+    // Exemplo de código usando a biblioteca Guzzle:
+    $client = new \GuzzleHttp\Client();
+    $response = $client->get('https://dbpedia.org/sparql', [
+        'query' => [
+            'query' => 'PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+                        PREFIX dbpedia: <http://dbpedia.org/resource/>
+
+                        SELECT ?abstract
+                        WHERE {
+                          dbpedia:' . $uf . ' dbpedia-owl:abstract ?abstract .
+                          FILTER (lang(?abstract) = "pt")
+                        }'
+        ],
+        'headers' => [
+            'Accept' => 'application/json'
+        ]
+    ]);
+
+    $data = json_decode($response->getBody(), true);
+
+    // Verificar se a resposta da DBpedia contém o resumo
+    if (isset($data['results']['bindings'][0]['abstract']['value'])) {
+        $resumo = $data['results']['bindings'][0]['abstract']['value'];
+
+        // Retornar o resumo como resposta JSON
+        return response()->json(['resumo' => $resumo]);
+    }
+
+    // Caso o resumo não seja encontrado, retornar uma resposta de erro
+    return response()->json(['error' => 'Resumo não encontrado'], 404);
+}
+     
+
 
     /**
      * Show the form for creating a new resource.
@@ -46,6 +91,7 @@ class UfController extends Controller
     {
         //
     }
+    
 
     /**
      * Show the form for editing the specified resource.
--- apicnpq/resources/views/ufs/create.blade.php
+++ apicnpq/resources/views/ufs/create.blade.php
@@ -1,20 +1,40 @@
 <x-layout title="Novo Uf">
-<div class="container" >
-    <h1>Cadastro de novo Uf</h1>
-    <p>Insira os dados abaixos para realizar o cadastro!</p>
-    <form action="/enderecos/ufs/salvar" method="post">
-        @csrf
-        <div class="form-group">
+    <div class="container">
+        <h1>Cadastro de novo Uf</h1>
+        <p>Insira os dados abaixo para realizar o cadastro!</p>
+        <form action="/enderecos/ufs/salvar" method="post">
+            @csrf
             <div class="form-group">
                 <label for="sigla">Uf: </label>
-                <input class="form-control" class="form-control" type="text" id="sigla" name="sigla">
+                <input class="form-control" type="text" id="sigla" name="sigla">
             </div>
-
-            
-        </div>
-        <div><br></div> 
-        <button class="btn btn-primary" type="submit" >Cadastrar</button>
-        <div><br></div> 
-    </form>
-</div>
+            <div class="form-group">
+                <label for="resumo">Resumo: </label>
+                <textarea class="form-control" id="resumo" name="resumo" readonly></textarea>
+            </div>
+            <div><br></div>
+            <button class="btn btn-primary" type="submit">Cadastrar</button>
+            <button class="btn btn-primary" type="button" onclick="consultarResumo()">Consultar Resumo</button>
+            <div><br></div>
+        </form>
+    </div>
 </x-layout>
+
+<script>
+    function consultarResumo() {
+        var uf = document.getElementById('sigla').value;
+        if (uf !== '') {
+            // Fazer a requisição AJAX para a rota no servidor
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var resumo = JSON.parse(this.responseText).resumo;
+                    document.getElementById('resumo').value = resumo;
+                }
+            };
+            xhttp.open('GET', '/enderecos/ufs/' + encodeURIComponent(uf) + '/resumo', true);
+            xhttp.send();
+        }
+    }
+</script>
+
